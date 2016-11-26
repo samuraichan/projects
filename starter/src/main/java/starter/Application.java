b40b22a9629f9549fb2426bf878ecebf1a6f15cd
samuraichan <rchiari@gmail.com>
2016-11-25 19:18:12 -0700

    root context to {/starter}

--- starter/src/main/java/starter/Application.java
+++ starter/src/main/java/starter/Application.java
@@ -1,4 +1,4 @@
-package myproject;
+package starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- starter/src/main/java/starter/web/GreetingController.java
+++ starter/src/main/java/starter/web/GreetingController.java
@@ -1,4 +1,4 @@
-package myproject.web;
+package starter.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
