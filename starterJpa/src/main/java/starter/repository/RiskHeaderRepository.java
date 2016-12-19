0cb9ebf424302c953c652704d4370db24db37f88
samuraichan <rchiari@gmail.com>
2016-12-18 17:51:28 -0700

    more test cases, specifically testing @version JPA functionality

--- starterJpa/src/main/java/starter/repository/RiskHeaderRepository.java
+++ starterJpa/src/main/java/starter/repository/RiskHeaderRepository.java
@@ -1,5 +1,6 @@
 package starter.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import starter.data.entity.RiskHeader;
@@ -20,5 +21,17 @@ public interface RiskHeaderRepository extends CrudRepository<RiskHeader, Integer
    */
   public RiskHeader findByNamedInsured(String namedInsured);
   
+  /**
+   * Find a <code>RiskHeader</code> by a primary identifier
+   * @param id
+   * @return
+   */
   public RiskHeader findById(Integer id);
+  
+  /**
+   * Find all <code>RiskHeader(s)</code> that are active
+   * @return
+   */
+  @Query("SELECT COUNT(risk) FROM RiskHeader risk WHERE risk.activeFlag = 'Y'")
+  public Long countByActive();
 }
--- starterJpa/src/test/java/starter/riskheader/RiskHeaderTest.java
+++ starterJpa/src/test/java/starter/riskheader/RiskHeaderTest.java
@@ -1,7 +1,9 @@
 package starter.riskheader;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import starter.data.entity.RiskHeader;
@@ -19,6 +22,8 @@ import starter.repository.RiskHeaderRepository;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class RiskHeaderTest {
+  
+  private static final String NAMED_INSURED = "Insurance Policy NP-1120";
   
   @Autowired
   private TestRestTemplate restTemplate;
@@ -32,22 +37,60 @@ public class RiskHeaderTest {
   }
   
   @Test
-  public void test() {
+  public void testHttpStatusOK() {
     assertThat(this.restTemplate.getForEntity(
       "/greeting", String.class, "??").getStatusCode()).isEqualTo(HttpStatus.OK);
   }
   
   @Test
-  public void insert() {
-    RiskHeader risk = new RiskHeader("my first one", RiskStatus.COMPLETED, true);
+  public void testRiskInsert() {
+    RiskHeader risk = new RiskHeader(NAMED_INSURED, RiskStatus.COMPLETED, true);
     assertThat(risk.getId()).isNull();
     
     risk = riskHeaderRepository.save(risk);
     assertThat(risk.getId()).isNotNull();
+  }
+  
+  @Test
+  public void testVersionUpdate() {
+    RiskHeader risk = riskHeaderRepository.findByNamedInsured("four");
+    risk.setStatusId(RiskStatus.DRAFT.getCode()); // status id is now dirty
+    int version = risk.getVersion();
+    risk = riskHeaderRepository.save(risk);
+    assertThat(riskHeaderRepository.save(risk).getVersion()).isEqualTo(version+1);
+  }
+  
+  @Test
+  public void testVersionNoUpdate() {
+    RiskHeader risk = riskHeaderRepository.findByNamedInsured("four");
+    int version = risk.getVersion();
+    risk = riskHeaderRepository.save(risk); // risk is not dirty, no update occurs
+    assertThat(riskHeaderRepository.save(risk).getVersion()).isEqualTo(version);
+  }
+  
+  @Test
+  public void testLockingException() {
+    RiskHeader risk = riskHeaderRepository.findByNamedInsured(NAMED_INSURED);
+    assertThat(risk.getNamedInsured()).isEqualTo(NAMED_INSURED);
     
-    risk = riskHeaderRepository.findByNamedInsured("my first one");
+    Throwable thrown = catchThrowable(new ThrowingCallable() {
+      @Override
+      public void call() throws Exception {
+        RiskHeader risk = riskHeaderRepository.findByNamedInsured(NAMED_INSURED);
+        risk.setVersion(risk.getVersion() - 1);
+        riskHeaderRepository.save(risk);
+      }
+    });
     
-    assertThat(risk).isNotNull();
-    assertThat(risk.getStatusId()).isEqualTo(RiskStatus.COMPLETED.getCode());
+    assertThat(thrown).isInstanceOf(ObjectOptimisticLockingFailureException.class);
+  }
+  
+  @Test
+  public void testActiveOnlyRecords() {
+    Long records = riskHeaderRepository.count();
+    riskHeaderRepository.save(new RiskHeader("My Man", RiskStatus.INPROGRESS, false));
+
+    assertThat(riskHeaderRepository.count()).isEqualTo(records+1);
+    assertThat(riskHeaderRepository.countByActive()).isEqualTo(records);
   }
 }
