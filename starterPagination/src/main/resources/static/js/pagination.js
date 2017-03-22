25e1c8e73d368de5514a0f0024fa241b8080c144
samuraichan <rchiari@gmail.com>
2017-03-21 18:09:40 -0600

    feature-starter-pagination: added error condition handling for datatables

--- starterPagination/src/main/resources/static/js/pagination.js
+++ starterPagination/src/main/resources/static/js/pagination.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	
+	$.fn.dataTable.ext.errMode = 'none';
+	
     var table = $('#pagination').DataTable( {
         "processing": true,
         "serverSide": true,
@@ -7,6 +9,7 @@ $(document).ready(function() {
         "ajax": {
           "url": "/starter/pagination",
           "type": "POST",
+          
           "data": function (d) {
             return $.extend( {}, d, {
               "searchFilter.statusId": $('#status option:selected').val(),
@@ -14,14 +17,21 @@ $(document).ready(function() {
               "searchFilter.endDate": $('#endDate').val(),
               "search": $('#mySearch').val()
             });
-          }
+          },
+          "dataSrc": function ( json ) {
+              // alert('you again');  // override this to see what results are in json, i will want to use jsonFormResponse and see how it goes, https://datatables.net/reference/option/ajax.dataSrc
+              return json.data;
+            }
         },
         "columns": [
           { data: 'namedInsured'},
           { data: 'status'},
           { data: 'createdDateString'}
         ]
-    });
+    }).on( 'error.dt', function (e, settings, techNote, message ) { // triggered when response is NOT of 2XX
+    	alert(message);
+        //console.log( 'An error has been reported by DataTables: ', message );
+    } );
     
     $('#btn').on('click', function() {
       table.draw();
